f6f386467d21f7ad3e9cc50299dcc85948178aeb
Revenant70 <jackmcg777@protonmail.com>
2025-06-09 15:56:13 -0400

    init

---
+++ src/main/java/org/example/carmaintenance/CarMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package org.example.carmaintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMaintenanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarMaintenanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/carmaintenance/CarMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.carmaintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMaintenanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
